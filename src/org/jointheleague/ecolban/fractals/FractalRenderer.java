7afea79fca1789cbdf17714db650972eb6bf4582
ecolban <erik.colban@drawmetry.com>
2015-09-28 18:25:57 -0700

    Added a print button. Added a new fractal spec.

--- src/org/jointheleague/ecolban/fractals/FractalRenderer.java
+++ src/org/jointheleague/ecolban/fractals/FractalRenderer.java
@@ -1,23 +1,29 @@
 package org.jointheleague.ecolban.fractals;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Toolkit;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Rectangle2D;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Random;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 @SuppressWarnings("serial")
-public class FractalRenderer extends JPanel {
+public class FractalRenderer extends JPanel implements Printable {
 
-//	private static final Dimension PREFERRED_DIMENSION = new Dimension(1020, 1020);
-	private static final Dimension PREFERRED_DIMENSION = new Dimension(1292, 1020);
+	// private static final Dimension PREFERRED_DIMENSION = new Dimension(1000,
+	// 1000);
 	private static final int MARGIN = 10;
 	private static final Random RNG = new Random();
 
@@ -25,20 +31,30 @@ public class FractalRenderer extends JPanel {
 
 	private FractalSpec fractal = new Test3();
 
-	public static void main(String[] args) throws InvocationTargetException, InterruptedException {
+	public static void main(String[] args) throws InvocationTargetException,
+	        InterruptedException {
 		FractalRenderer f = new FractalRenderer();
 		SwingUtilities.invokeAndWait(() -> f.buildGui());
 		for (int i = 0; i <= f.fractal.getLevel(); i++) {
 			f.level = i;
 			f.repaint();
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 		}
 	}
 
 	private void buildGui() {
 		JFrame frame = new JFrame();
-		setPreferredSize(PREFERRED_DIMENSION);
-		frame.add(this);
+		frame.setLayout(new BorderLayout());
+		double h = Toolkit.getDefaultToolkit().getScreenSize().getHeight() - 2
+		        * MARGIN - 50;
+		double w = h * 1.272;
+		setPreferredSize(new Dimension((int) w, (int) h));
+		frame.add(this, BorderLayout.CENTER);
+		JPanel buttonPanel = new JPanel();
+		JButton printButton = new JButton("Print");
+		printButton.addActionListener(e -> printFractal());
+		buttonPanel.add(printButton);
+		frame.add(buttonPanel, BorderLayout.SOUTH);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
@@ -51,10 +67,11 @@ public class FractalRenderer extends JPanel {
 		g2.fillRect(0, 0, getWidth(), getHeight());
 		g2.translate(MARGIN, MARGIN);
 		g2.scale(getWidth() - 2 * MARGIN, getHeight() - 2 * MARGIN);
-		fillFractal(fractal.getTransforms(), g2, level);
+		fillFractal(g2, fractal.getTransforms(), level);
 	}
 
-	private void fillFractal(AffineTransform[] transforms, Graphics2D g2, int level) {
+	private void fillFractal(Graphics2D g2, AffineTransform[] transforms,
+	        int level) {
 		if (level == 0) {
 			g2.setColor(randomColor());
 			g2.fillRect(0, 0, 1, 1);
@@ -62,7 +79,7 @@ public class FractalRenderer extends JPanel {
 			AffineTransform cachedTransform = g2.getTransform();
 			for (AffineTransform t : transforms) {
 				g2.transform(t);
-				fillFractal(transforms, g2, level - 1);
+				fillFractal(g2, transforms, level - 1);
 				g2.setTransform(cachedTransform);
 			}
 		}
@@ -70,6 +87,34 @@ public class FractalRenderer extends JPanel {
 
 	private Color randomColor() {
 		return new Color(RNG.nextInt(256), RNG.nextInt(256), RNG.nextInt(256));
+	}
+
+	private void printFractal() {
+		PrinterJob job = PrinterJob.getPrinterJob();
+		job.setPrintable(this);
+		boolean ok = job.printDialog();
+		if (ok) {
+			try {
+				job.print();
+			} catch (PrinterException ex) {
+				/* The job did not successfully complete */
+			}
+		}
+	}
+
+	@Override
+	public int print(Graphics g, PageFormat pf, int page)
+	        throws PrinterException {
+		if (page > 0) {
+			return NO_SUCH_PAGE;
+		}
+
+		Graphics2D g2 = (Graphics2D) g;
+		g2.translate(pf.getImageableX(), pf.getImageableY());
+		g2.scale(pf.getImageableWidth(), pf.getImageableHeight());
+		fillFractal(g2, fractal.getTransforms(), fractal.getLevel());
+
+		return PAGE_EXISTS;
 	}
 
 }
--- src/org/jointheleague/ecolban/fractals/Test4.java
+++ src/org/jointheleague/ecolban/fractals/Test4.java
@@ -4,17 +4,18 @@ import java.awt.geom.AffineTransform;
 
 import static java.lang.Math.sqrt;
 
-public class Test3 implements FractalSpec {
-	// Set the panel dimension such that width == k * height, 
-	// where k == sqrt(1 + golden) after removing margins.
+public class Test4 implements FractalSpec {
+	// Set the panel dimension such that the width is k * the height, where k = sqrt(1 + golden) 
+	// after removing margins.
 
 	@Override
 	public AffineTransform[] getTransforms() {
 		double golden = (-1.0 + sqrt(5.0)) / 2.0;
-		double golden2 = golden * golden;
+		double golden2 = 1.0 - golden; // also == golden * golden
 		return new AffineTransform[] {
 		        new AffineTransform(0.0, -1.0, golden, 0, 0.0, 1.0),
-		        new AffineTransform(0.0, golden, -golden2, 0.0, 1.0, golden2),
+//		        new AffineTransform(0.0, golden, -golden2, 0.0, 1.0, golden2),
+		        new AffineTransform(golden2, 0.0, 0.0, golden2, golden, 0.0)
 		};
 	}
 
